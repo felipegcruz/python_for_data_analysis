fdaa7be05d196dba3b1e1dc21ae23b0ea94b5328
Felipe Cruz <felipegcruzdev@gmail.com>
2023-08-20 19:32:24 +0100

    classes in python

---
+++ 01_Python_Basics/Building the Car Game/car_game.py
@@ -0,0 +1,18 @@
+command = ""
+
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        print("Car started...")
+    elif command == "stop":
+        print("Car stopped")
+    elif command == "help":
+        print("""
+start - to start the car
+stop - to stop the car
+quit - to quit
+        """)
+    elif command == "quit":
+        break
+    else:
+        print("Sorry I do not understand that")
